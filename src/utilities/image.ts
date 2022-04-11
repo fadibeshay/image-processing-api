2ad1a98cd89b0b742afb7f8a1e182ba1b0109fd7
Fadi Beshay <fady.ayad.90@gmail.com>
2022-04-11 11:50:24 +0200

    feat: Add utilities/image resize function

--- src/utilities/image.ts
+++ src/utilities/image.ts
@@ -1,9 +1,36 @@
 import sharp from 'sharp';
+import path from 'path';
+import { promises as fsPromises } from 'fs';
 
 async function resize(
   imageName: string,
   width: number,
   height: number
-): Promise<void> {}
+): Promise<string> {
+  const imagePath = path.join(
+    __dirname,
+    '..',
+    '..',
+    'full',
+    `${imageName}.jpg`
+  );
+
+  const thumbPath = path.join(
+    __dirname,
+    '..',
+    '..',
+    'thumb',
+    `${imageName}-${width}x${height}.jpg`
+  );
+
+  try {
+    // If thumbnail is found, do not recreate it
+    await fsPromises.access(thumbPath);
+  } catch (error) {
+    // If thumbnail is not found, create it first
+    await sharp(imagePath).resize(width, height).toFile(thumbPath);
+  }
+  return thumbPath;
+}
 
 export default { resize };
