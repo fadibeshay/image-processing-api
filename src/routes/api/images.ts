5215259ac5725dcc07026ee2fe732429b4ff6ea5
Fadi Beshay <fady.ayad.90@gmail.com>
2022-04-11 12:16:56 +0200

    refactor: Add functionality to GET /api/images endpoint to pass the tests

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,33 @@
 import express from 'express';
+import image from '../../utilities/image';
 
 const images = express.Router();
 
-images.get('/', async (_req, res) => {
-  res.send('/api/images route');
+images.get('/', async (req, res) => {
+  const filename = req.query.filename as string | undefined;
+  const width = parseInt(req.query.width as string);
+  const height = parseInt(req.query.height as string);
+
+  if (!filename) {
+    return res.status(400).json({ error: 'filename is missing' });
+  }
+  if (!(width > 0)) {
+    return res
+      .status(400)
+      .json({ error: 'width is missing or not a positive number' });
+  }
+  if (!(height > 0)) {
+    return res
+      .status(400)
+      .json({ error: 'height is missing or not a positive number' });
+  }
+
+  try {
+    const thumb = await image.resize(filename, width, height);
+    res.sendFile(thumb);
+  } catch (error) {
+    return res.status(404).json({ error: 'image not found' });
+  }
 });
 
 export default images;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import images from './api/images';
 
 const routes = express.Router();
 
 routes.get('/', (_req, res) => {
   res.send('Main API route');
 });
+
+routes.use('/images', images);
 
 export default routes;
